34dc17f8ea04a0ba9c1fdf171ca57f4c24fe85bb
igor ricci <assis.ricci@gmail.com>
2022-07-02 01:01:35 -0300

    tomato can move

--- app.js
+++ app.js
@@ -5,22 +5,31 @@ const squares = document.querySelectorAll('.grid div')
 
 console.log(squares);
 let currentSquare = 76;
+const step = 9;
 
 function moveFrog(e) {
+
+  squares[currentSquare].classList.remove('tomato')
 
   switch (e.key) {
     case 'ArrowLeft':
       console.log('move left');
+      currentSquare -= 1;
       break
     case 'ArrowRight':
       console.log('move right');
+      currentSquare += 1;
       break
     case 'ArrowUp':
       console.log('move Up');
+      currentSquare -= step;
       break
     case 'ArrowDown':
       console.log('move Down');
+      currentSquare += step;
       break
+    default:
+      console.log('invalid key');
 
 
   }
