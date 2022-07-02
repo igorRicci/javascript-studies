b991c8deb1c067be65a4ef028c3b1aa5fcd5a790
igor ricci <assis.ricci@gmail.com>
2022-07-02 14:46:10 -0300

    logs moving in opposite directions

--- app.js
+++ app.js
@@ -45,6 +45,9 @@ function autoMoveLogs() {
   logsLeft.forEach((logLeft) => {
     moveLogLeft(logLeft)
   })
+  logsRight.forEach((logRight) => {
+    moveLogRight(logRight)
+  })
 }
 
 autoMoveLogs
@@ -70,6 +73,31 @@ function moveLogLeft(logLeft) {
     case logLeft.classList.contains('l5') :
       logLeft.classList.remove('l5')
       logLeft.classList.add('l1')
+      break
+  }
+}
+
+function moveLogRight(logRight) {
+  switch (true) {
+    case logRight.classList.contains('l1') :
+      logRight.classList.remove('l1')
+      logRight.classList.add('l5')
+      break
+    case logRight.classList.contains('l2') :
+      logRight.classList.remove('l2')
+      logRight.classList.add('l1')
+      break
+    case logRight.classList.contains('l3') :
+      logRight.classList.remove('l3')
+      logRight.classList.add('l2')
+      break
+    case logRight.classList.contains('l4') :
+      logRight.classList.remove('l4')
+      logRight.classList.add('l3')
+      break
+    case logRight.classList.contains('l5') :
+      logRight.classList.remove('l5')
+      logRight.classList.add('l4')
       break
   }
 }
